2685bb0fc4aad10f7148664865c3b982520e6b06
matthias-grgic <85082249+matthias-grgic@users.noreply.github.com>
2022-01-11 20:41:40 +0100

    casesensitivity 2

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 import express from "express"
-import cors from "cors"
+// import cors from "cors"
 import mongoose from "mongoose"
 import ServerRoutes from "./routes/server.routes.js"
 import dotenv from "dotenv"
@@ -7,29 +7,14 @@ import { fileURLToPath } from "url"
 import { dirname as dirnameFromPath } from "path"
 import path from "path"
 
+dotenv.config()
+
 function dirname(importMetaUrl) {
     return fileURLToPath(dirnameFromPath(importMetaUrl))
 }
 const __dirname = dirname(import.meta.url)
 
-dotenv.config()
-
-const server = express()
-const Port = process.env.SERVER_PORT || 4000
-
-//MIDDLEWARE
-server.use(express.json())
-server.use(cors())
-
-// Serve static files from the React app
-server.use(express.static(path.join(__dirname, "../client/dist")))
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-
-// server.get("/*", (_req, res) => {
-//     res.sendFile(path.join(__dirname, "../client/dist", "index.html"))
-// })
+const PORT = process.env.PORT || 4000
 
 //DATABASE
 const DB_USER = process.env.DB_USER
@@ -40,8 +25,22 @@ const DB_NAME = process.env.DB_NAME
 const connectionString = `mongodb+srv://${DB_USER}:${DB_PASSWORD}${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`
 mongoose.connect(connectionString)
 
+const server = express()
+//MIDDLEWARE
+server.use(express.json())
+// server.use(cors())
+
+// Serve static files from the React app
+server.use(express.static(path.join(__dirname, "../client/dist")))
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+// server.get("/*", (_req, res) => {
+//     res.sendFile(path.join(__dirname, "../client/dist", "index.html"))
+// })
+
 //Routes
 server.use("/", ServerRoutes)
 
 //RUN SERVER
-server.listen(Port, () => console.log("Server is running"))
+server.listen(PORT, () => console.log("Server is running"))
