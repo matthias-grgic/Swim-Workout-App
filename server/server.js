5027bf50b48caab13f788bf88f79233ec9fb1a21
matthias-grgic <85082249+matthias-grgic@users.noreply.github.com>
2022-01-11 19:55:20 +0100

    ...

--- server/server.js
+++ server/server.js
@@ -26,9 +26,10 @@ server.use(express.static(path.join(__dirname, "../client/dist")))
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-server.get("/*", (_req, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist", "index.html"))
-})
+
+// server.get("/*", (_req, res) => {
+//     res.sendFile(path.join(__dirname, "../client/dist", "index.html"))
+// })
 
 //DATABASE
 const DB_USER = process.env.DB_USER
