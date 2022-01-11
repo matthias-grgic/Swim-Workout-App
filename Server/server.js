321fa36a2839cb2dc29f21e98d5136e54c7600af
matthias-grgic <85082249+matthias-grgic@users.noreply.github.com>
2022-01-11 16:12:56 +0100

    mongodb cloud working

--- Server/server.js
+++ Server/server.js
@@ -5,14 +5,20 @@ import ServerRoutes from "./routes/server.routes.js"
 import dotenv from "dotenv"
 
 const server = express()
+dotenv.config()
 const Port = process.env.SERVER_PORT || 4000
 
 //MIDDLEWARE
 server.use(express.json())
 server.use(cors())
 
-// dotenv.config()
-const connectionString = "mongodb://localhost:27017/exerciseTest"
+//DATABASE
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_HOST = process.env.DB_HOST
+const DB_NAME = process.env.DB_NAME
+
+const connectionString = `mongodb+srv://${DB_USER}:${DB_PASSWORD}${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`
 mongoose.connect(connectionString)
 
 //Routes
