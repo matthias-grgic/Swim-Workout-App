e3f412944346ae10dcbdf5c8c537e57f3248c06e
matthias-grgic <85082249+matthias-grgic@users.noreply.github.com>
2022-01-12 10:57:29 +0100

    umbau füer heroku

--- server/server.js
+++ server/server.js
@@ -1,22 +1,14 @@
+import cors from "cors"
 import express from "express"
-// import cors from "cors"
 import mongoose from "mongoose"
-import ServerRoutes from "./routes/server.routes.js"
-import dotenv from "dotenv"
-import { fileURLToPath } from "url"
-import { dirname as dirnameFromPath } from "path"
 import path from "path"
+import dotenv from "dotenv"
+import ServerRoutes from "./routes/server.routes.js"
 
 dotenv.config()
 
-function dirname(importMetaUrl) {
-    return fileURLToPath(dirnameFromPath(importMetaUrl))
-}
-const __dirname = dirname(import.meta.url)
-
 const PORT = process.env.PORT || 4000
 
-// DATABASE
 const dbUser = process.env.dbUser
 const dbPassword = process.env.dbPassword
 const dbHost = process.env.dbHost
@@ -25,22 +17,20 @@ const dbName = process.env.dbName
 const connectionString = `mongodb+srv://workoutadmin2000:BBpksjDuGqUS4f5@cluster0.4vd0c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 mongoose.connect(connectionString)
 
+const __dirname = process.cwd()
+
 const server = express()
-//MIDDLEWARE
+server.use(cors())
 server.use(express.json())
-// server.use(cors())
-
-// Serve static files from the React app
-server.use(express.static(path.join(__dirname, "../client/dist")))
-
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-// server.get("/*", (_req, res) => {
-//     res.sendFile(path.join(__dirname, "../client/dist", "index.html"))
-// })
 
 //Routes
 server.use("/", ServerRoutes)
 
+server.use(express.static(path.join(__dirname, "./client/dist")))
+
+server.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/dist", "index.html"))
+})
+
 //RUN SERVER
-server.listen(PORT, () => console.log("Server is running"))
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
