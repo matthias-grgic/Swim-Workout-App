6aa351b1d3836ea679bfdd4b715fc6cd4ea4ca13
matthias-grgic <85082249+matthias-grgic@users.noreply.github.com>
2022-01-13 18:49:44 +0100

    setting page update

--- client/src/components/Settings.jsx
+++ client/src/components/Settings.jsx
@@ -1,7 +1,30 @@
 import styled from "styled-components"
 
 function Settings() {
-    return <div>Settings</div>
+    return (
+        <MainDiv>
+            <div>Settings</div>
+            <Infos>
+                <p>Sources</p>
+                <p>SpeedoInternational @Youtube</p>
+                <p>...</p>
+            </Infos>
+        </MainDiv>
+    )
 }
 
 export default Settings
+
+const MainDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+`
+
+const Infos = styled.div`
+    display: flex;
+    flex-direction: column;
+    color: #a3a3a3;
+`
