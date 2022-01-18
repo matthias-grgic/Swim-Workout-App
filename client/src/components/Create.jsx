0efa41189dd7a8f3947536f36a0ec0be3af6a8e0
matthias-grgic <85082249+matthias-grgic@users.noreply.github.com>
2022-01-18 14:56:47 +0100

    formular is working

--- client/src/components/Create.jsx
+++ client/src/components/Create.jsx
@@ -1,39 +1,66 @@
 import styled from "styled-components"
 import { useState } from "react"
+import AddButton from "../images/addbutton.svg"
 
 function Create() {
-    const [status, setStatus] = useState()
+    const [name, setName] = useState("")
+    const [type, setType] = useState("")
+    const [equipment, setEquipment] = useState("")
+    const [video, setVideo] = useState("")
+    const [definition, setDefinition] = useState("")
 
-    const handleChange = (event) => setStatus(event.target.value)
-    const handleSubmit = (event) => {
-        alert(`New Exercise "${status}" added`)
-        event.preventDefault()
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const settings = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name: name,
+                    type: type,
+                    video: video,
+                    definition: definition,
+                    equipment: equipment,
+                }),
+            }
+            const fetchResponse = await fetch("/api/postexerciselist", settings)
+            const data = await fetchResponse.json()
+            return data
+        } catch (e) {
+            console.error(e)
+        }
     }
 
     return (
         <MainDiv>
-            <h2>ADD EXERCISE</h2>
+            <h1>ADD EXERCISE</h1>
             <Form onSubmit={handleSubmit}>
                 <FormField>
-                    <label htmlFor="name">Name</label>
-                    <input id="name" value={status} onChange={handleChange} type="text" name="name" placeholder="Exercise" />
+                    <input id="name" value={name} onChange={(e) => setName(e.target.value)} type="text" name="name" placeholder="Exercise" />
                 </FormField>
                 <FormField>
-                    <label htmlFor="type">Type</label>
-                    <select name="type">
-                        <option value="normal">normal</option>
-                        <option value="drill">drill</option>
+                    <select name="type" value={type} onChange={(e) => setType(e.target.value)}>
+                        <option value="normal">Normal</option>
+                        <option value="drill">Drill</option>
                     </select>
                 </FormField>
                 <FormField>
-                    <label htmlFor="name">Video</label>
-                    <input type="text" name="color" placeholder="Link" />
+                    <select name="equipment" value={equipment} onChange={(e) => setEquipment(e.target.value)}>
+                        <option value="pullbuoy">Pullbuoy</option>
+                        <option value="paddles">Paddles</option>
+                        <option value="snorkel">Snorkel</option>
+                        <option value="fins">Fins</option>
+                    </select>
                 </FormField>
                 <FormField>
-                    <label htmlFor="subject">Definition</label>
-                    <textarea id="subject" name="subject" placeholder="..."></textarea>
+                    <input type="text" name="video" value={video} onChange={(e) => setVideo(e.target.value)} placeholder="Youtube Link" />
                 </FormField>
-                <input type="submit" value="SUBMIT" />
+                <FormField>
+                    <textarea id="subject" name="definition" value={definition} onChange={(e) => setDefinition(e.target.value)} placeholder="Definition"></textarea>
+                </FormField>
+                <input type="submit" value="" />
             </Form>
         </MainDiv>
     )
@@ -47,55 +74,53 @@ const MainDiv = styled.div`
     gap: 10px;
     margin-bottom: 40px;
     width: 70%;
+    h2 {
+        margin-bottom: 10px;
+    }
 `
 
 const Form = styled.form`
     display: flex;
     flex-direction: column;
     font-size: 1rem;
-
+    gap: 10px;
     input[type="text"],
     select,
     textarea {
+        box-shadow: 0;
         border: 1px solid #ccc;
         border-radius: 4px;
         display: inline-block;
         margin: 0px 0;
         padding: 12px 20px;
-        width: 100%;
     }
 
     input[type="submit"] {
         transition: all 0.1s ease-in-out;
-        background-image: linear-gradient(-225deg, #7de2fc 0%, #b9b6e5 100%);
-        &:active {
-            background-image: linear-gradient(-225deg, #c6eef8 0%, #b9b6e5 100%);
-            color: white;
-        }
-        &:hover {
-            background-image: linear-gradient(-225deg, #c6eef8 0%, #b9b6e5 100%);
-            transition: all 1s ease;
-            color: white;
-        }
-        box-shadow: var(--main-box-shadow);
+        background-color: transparent;
+        background-image: url(${AddButton});
+        background-size: 55px;
+        background-repeat: no-repeat;
+        background-position: center;
         border: none;
         border-radius: 4px;
         color: white;
         cursor: pointer;
         margin: 15px 0;
-        padding: 14px 20px;
+        padding: 30px;
         width: 100%;
+        &:active {
+            color: white;
+        }
+
+        &:hover {
+            transition: all 1s ease;
+        }
     }
 
     textarea {
         height: 100px;
         resize: none;
-    }
-
-    label {
-        margin-bottom: 2px;
-        margin-top: 15px;
-        width: 100px;
     }
 `
 
