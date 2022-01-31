cf19579a77f5232f1c2238f7222edbada7767753
matthias-grgic <85082249+matthias-grgic@users.noreply.github.com>
2022-01-31 19:05:00 +0100

    redirect and scroll to object working. installed react router hash for this function

--- client/src/components/MapWorkout.jsx
+++ client/src/components/MapWorkout.jsx
@@ -4,11 +4,9 @@ import FinsImg from "../images/equipment/fins.svg"
 import HandPaddleImg from "../images/equipment/handpaddles.svg"
 import PullbuoyImg from "../images/equipment/pullbuoy.svg"
 import SnorkelImg from "../images/equipment/snorkel.svg"
-import { useNavigate } from "react-router-dom"
+import { HashLink } from "react-router-hash-link"
 
 export default function MapCurrentWod({ main, switchOne, switchTwo, lengthOfWod, setExpandedAccordion }) {
-    const navigate = useNavigate()
-
     const typeLengthCheck = (type, length) => {
         if (type === "drill") {
             return halfAmount(switchOne, 4) + " x " + doubleLap(switchOne, length) + "m"
@@ -18,38 +16,44 @@ export default function MapCurrentWod({ main, switchOne, switchTwo, lengthOfWod,
         }
     }
 
-    const handleClick = (item) => {
-        navigate("/ExerciseList")
+    const RedirectAndExpand = (item) => {
         setExpandedAccordion(item)
     }
 
     return main.map((item, index, arr) => {
         if (switchTwo === false && lengthOfWod === 0) {
             return (
-                <ExerciseCards onClick={() => handleClick(item.id)} key={index}>
-                    <ExerciseCardsTitle>{item.name}</ExerciseCardsTitle>
-                    <IMGDiv value={item.equipment} />
-                    <>1 x</>
-                    <p>200m</p>
-                </ExerciseCards>
+                <HashLink key={index} onClick={() => RedirectAndExpand(item.id)} smooth to={`/ExerciseList#${item.id}`}>
+                    <ExerciseCards>
+                        <ExerciseCardsTitle>{item.name}</ExerciseCardsTitle>
+                        <IMGDiv value={item.equipment} />
+                        <>1 x</>
+                        <p>200m</p>
+                    </ExerciseCards>
+                </HashLink>
             )
         } else if (arr.length - 1 === index && switchTwo === false && lengthOfWod === 50) {
             return (
-                <ExerciseCards onClick={() => handleClick(item.id)} key={index}>
-                    <ExerciseCardsTitle>{item.name}</ExerciseCardsTitle>
-                    <IMGDiv value={item.equipment} />
-                    <>1 x</>
-                    <p>100m</p>
-                </ExerciseCards>
+                // <ExerciseCards onClick={() => RedirectAndExpand(item.id)} key={index}>
+                <HashLink key={index} onClick={() => RedirectAndExpand(item.id)} smooth to={`/ExerciseList#${item.id}`}>
+                    <ExerciseCards>
+                        <ExerciseCardsTitle>{item.name}</ExerciseCardsTitle>
+                        <IMGDiv value={item.equipment} />
+                        <>1 x</>
+                        <p>100m</p>
+                    </ExerciseCards>
+                </HashLink>
             )
         }
         {
             return (
-                <ExerciseCards onClick={() => handleClick(item.id)} key={index}>
-                    <ExerciseCardsTitle>{item.name}</ExerciseCardsTitle>
-                    <IMGDiv value={item.equipment} />
-                    <>{typeLengthCheck(item.type, item.length)}</>
-                </ExerciseCards>
+                <HashLink key={index} onClick={() => RedirectAndExpand(item.id)} smooth to={`/ExerciseList#${item.id}`}>
+                    <ExerciseCards>
+                        <ExerciseCardsTitle>{item.name}</ExerciseCardsTitle>
+                        <IMGDiv value={item.equipment} />
+                        <>{typeLengthCheck(item.type, item.length)}</>
+                    </ExerciseCards>
+                </HashLink>
             )
         }
     })
@@ -57,6 +61,7 @@ export default function MapCurrentWod({ main, switchOne, switchTwo, lengthOfWod,
 
 const ExerciseCards = styled.div`
     border-bottom: ${(props) => (props.noBorder ? "0px solid var(--border-seperator)" : "1px solid var(--border-seperator);")};
+    cursor: pointer;
     display: flex;
     justify-content: space-between;
     gap: 5px;
